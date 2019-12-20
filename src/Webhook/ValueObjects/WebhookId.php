ac847959ce46c0424b6f59571ce22fd0beeaa961
Wallace Ferreira <wallace.sf87@gmail.com>
2019-12-20 13:17:27 -0300

    refactory

--- src/Webhook/ValueObjects/WebhookId.php
+++ src/Webhook/ValueObjects/WebhookId.php
@@ -7,7 +7,7 @@ use Mundipagg\Core\Kernel\ValueObjects\AbstractValidString;
 class WebhookId extends AbstractValidString
 {
     protected function validateValue($value)
-    {
-        return preg_match('/hook_\w{16}$/', $value) === 1;
+    {   $preg = preg_match('/hook_\w{16}$/', $value) === 1;
+        return $preg;
     }
 }
